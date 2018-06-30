f8416ce1dd96e4d25697c837977f85c16f694e16
Daniel <ddazaj@uninorte.edu.co>
2018-06-29 19:31:55 -0500

    adapt home imgs

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
         <div className="row">
           <div className="col-sm-5">
             <div className="container">
-              <img src={ circuit } alt="Circuit Background" className="img-bg" width="400px" height="400px" ></img>
+              <img src={ circuit } alt="Circuit Background" className="img-bg" width="80%" height="50%" ></img>
               <h1 className="description top-left" >
                 Technology and healthcare, advancing together towards a better, kinder future
               </h1>
@@ -22,7 +22,7 @@ class Home extends Component {
           </div>
           <div className="col-sm-5">
             <div className="poster">
-              <img src={ img } width="400px" height="400px" alt="Main poster" />
+              <img src={ img } width="80%" height="80%" alt="Main poster" />
             </div>
           </div>
         </div>
