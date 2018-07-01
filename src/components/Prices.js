2e1fa037bd2d731f4973c7028166404ce0e78262
Daniel <ddazaj@uninorte.edu.co>
2018-07-01 15:41:07 -0500

    correct prices page

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -14,24 +14,47 @@ class Prices extends Component {
 
         <div className="row">
           <div className="col">
-            <div class="card" style="width: 18rem;">
+            <div class="card bg-warning">
               <div class="card-body">
                 <h5 class="card-title">CEM</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Cem Members</h6>
+                <hr className="my-4"/>
                 <p class="card-text">XXXXXX</p>
               </div>
             </div>
           </div>
-          
+
           <div className="col">
-            <div class="card" style="width: 18rem;">
+            <div class="card bg-warning">
               <div class="card-body">
-                <h5 class="card-title">IEEE</h5>
+                <h5 class="card-title ">IEEE</h5>
                 <h6 class="card-subtitle mb-2 text-muted">IEEE Members</h6>
+                <hr className="my-4"/>
                 <p class="card-text">XXXXXX</p>
               </div>
             </div>
           </div>
+
+          <div className="col">
+            <div class="card bg-warning">
+              <div class="card-body">
+                <h5 class="card-title ">Students</h5>
+                <hr className="my-4"/>
+                <p class="card-text">XXXXXX</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            <div class="card bg-warning">
+              <div class="card-body">
+                <h5 class="card-title ">Profesionals</h5>
+                <hr className="my-4"/>
+                <p class="card-text">XXXXXX</p>
+              </div>
+            </div>
+          </div>
+
         </div>
 
         <br/>
